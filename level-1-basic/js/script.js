0fff418d874c7a1790022530b9811b10ecb634ed
0101Keke <kekeletsomaleka04@gmail.com>
2026-02-01 22:00:20 +0200

    feat: dropdown for skills filter

--- level-1-basic/js/script.js
+++ level-1-basic/js/script.js
@@ -4,3 +4,20 @@ function toggleMenu(){
    menu.classList.toggle('open');
    icon.classList.toggle('open');
 }
+
+
+// Skills Filter Functionality
+const skillsFilter = document.getElementById('skillsFilter');
+const skillsGroup = document.getElementsByClassName('skills-group');
+
+skillsFilter.addEventListener("change", () => {
+    const selectedValue = skillsFilter.value;
+
+    Array.from(skillsGroup).forEach(group => {
+        if (selectedValue === "all" || group.classList.contains(selectedValue)) {
+            group.style.display = "block";
+        } else {
+            group.style.display = "none";
+        }
+    });
+});
