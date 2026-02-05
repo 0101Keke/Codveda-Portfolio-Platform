36de2f509c50802b0dec1d70d882ec0d98ada2f0
0101Keke <kekeletsomaleka04@gmail.com>
2026-02-06 00:11:47 +0200

    Cursor Animation
    I tried animating the cursor, I still need to fix it as it works on the upper side of the page until you scroll then it is a problem, the circles are not fixed onto the cursor.

--- level-1-basic/js/script.js
+++ level-1-basic/js/script.js
@@ -173,3 +173,50 @@ themeToggle.addEventListener("click", () => {
 });
 
 
+//Cursor Animation
+const coords = {x: 0, y:0};
+const circles = document.querySelectorAll(".circle");
+
+
+circles.forEach(function (circle) {
+  circle.x = 0;
+  circle.y = 0;
+});
+
+window.addEventListener("mousemove", function(e) {
+  //console.log(e.clientX, e.clientY);
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+
+  //console.log(coords);
+
+});
+
+function animateCircles() {
+
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function(circle, index) {
+    circle.style.left = coords.x - 12 + "px";
+    circle.style.top = coords.y - 12 + "px";
+
+    circle.style.scale = (circles.length -index) / circles.length;
+ 
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+
+    //x += 10;
+    //y += 10;
+  });
+
+  requestAnimationFrame(animateCircles)
+}
+
+animateCircles();
+
+
